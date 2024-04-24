67b33c7fda1eda27df3360f893ab388e573b192b
Tim <36696969+Balestrini1011@users.noreply.github.com>
2024-04-24 19:43:56 +0100

    Add files via upload

---
+++ separateWheatFromChaff.js
@@ -0,0 +1,9 @@
+function wheatFromChaff(values) {
+    for (let head = 0, tail = values.length - 1; head < tail; ) {
+        while (values[head] < 0) head++;
+        while (values[tail] > 0) tail--;
+        if (head < tail)
+          [values[tail], values[head]] = [values[head], values[tail]];
+      }
+      return values;
+    }
